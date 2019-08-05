3f7c8800acfad16e9c589ecdecc0c8e9f1b62969
sunnyzhang06 <sunnyzhang9205@gmail.com>
2019-08-05 17:02:37 +0800

    first commit

---
+++ utility.h
@@ -0,0 +1,20 @@
+#ifndef _UTILITY_H_
+#define _UTILITY_H_
+
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+using namespace std;
+
+
+
+#endif
+
