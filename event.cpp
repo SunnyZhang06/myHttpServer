2f242c50c78379e5c618ed81b46a449e7c521942
sunnyzhang06 <sunnyzhang9205@gmail.com>
2019-08-06 16:13:59 +0800

    add epoll

--- event.cpp
+++ event.cpp
@@ -3,7 +3,7 @@
 
 void CEvent::init()
 {
-	int listenfd = socket(AF_INET,SOCK_STREAM,0);
+	listenfd = socket(AF_INET,SOCK_STREAM,0);
 	struct sockaddr_in serv_addr;
 	bzero(&serv_addr,sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
