6dc71bbbaa6ff46a9ab064a78404718711c2df68
sunnyzhang06 <sunnyzhang9205@gmail.com>
2019-08-09 11:10:55 +0800

    amend event

--- event.cpp
+++ event.cpp
@@ -74,39 +74,6 @@ void CEvent::handle_accept(int epfd,int listenfd)
 	add_fd(epfd,client_fd);
 }
 
-void CEvent::handle_request(int epfd,int fd)
-{
-	char buffer[1024*1024];
-	int nread = read(fd,buffer,sizeof(buffer));
-	printf("读到的请求是%s\n",buffer);
- 
-	char filename[10] = {0};
-	sscanf(buffer,"GET /%s",filename);
-	printf("解析的文件名是：%s\n",filename);
- 
-	char* mime = NULL;
-	if(strstr(filename,".html"))
-		mime = "text/html";
-	else if(strstr(filename,".jpg"))
-		mime = "image/jpg";
-	
-	//响应报文段
-	char response[1024*1024];
-	sprintf(response,"HTTP/1.1 200 OK\r\nContext-Type: %s\r\n\r\n",mime);
-	int headlen = strlen(response);
-	
-	//打开客户请求的文件并写进响应报文中
-	int filefd = open(filename,O_RDONLY);
-	int filelen = read(filefd,response+headlen,sizeof(response)-headlen);
-	
-	//把响应报文段发送给客户端
-	write(fd,response,headlen+filelen);
-	epoll_ctl(epfd,EPOLL_CTL_DEL,fd,NULL);
-	close(fd);
-	close(filefd); 
-}
- 
-
 void CEvent::do_epoll()
 {
 	epfd = epoll_create(EPOLL_CREATE);
